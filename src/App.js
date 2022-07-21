e7927e356cb607d2a70fa461dc82b389c13b2665
Tarandeep Singh <65854945+Tarandeep-s1ngh@users.noreply.github.com>
2022-07-21 12:24:32 +0530

    fix: scroll issue

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import { Bookmarks, Explore, Feed, Profile, Signin, Signup } from "./pages";
 
 function App() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div className="App text-gray-800">
       <Routes>
