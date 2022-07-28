c875815bd70b1b10b8b27e93249dae64e1ece4aa
Tarandeep Singh <65854945+Tarandeep-s1ngh@users.noreply.github.com>
2022-07-28 17:17:30 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
@@ -10,13 +10,14 @@ import "./index.css";
 // Call make Server
 makeServer();
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
